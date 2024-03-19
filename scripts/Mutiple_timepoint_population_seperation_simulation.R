949112b37f08a138655bb652642ff4cc5c65449e
Avi ~ Uddeshya Pandey <77080877+UddeshyaPandey@users.noreply.github.com>
2024-03-19 16:51:17 +0100

    Update Mutiple_timepoint_population_seperation_simulation.R

--- scripts/Mutiple_timepoint_population_seperation_simulation.R
+++ scripts/Mutiple_timepoint_population_seperation_simulation.R
@@ -1,5 +1,10 @@
+#In this in-silico simulation for T cell population separation into two predominant experimental engagement states enagegers and non engagers,
+# which is collectively called as t cell contacts with the Tumor organoid. 
+# The purpose of this simulation is to recreate a T cell and tumor organoid co culture experiment and,
+# for the different user defined time intervals find out the behavioral signatures frequencies of the t cells in engaged and non engaged states.
+
 population_separation_simulation <- function(working_directory_path, classified_tcell_track_data_filepath_rds, n_timepoints, first_timepoint_intervals, reoccuring_time) {
-  library(tidyverse)
+  library(tidyverse) 
   library(fs)
   library(furrr)
   library(ggplot2)
@@ -13,10 +18,13 @@ population_separation_simulation <- function(working_directory_path, classified_
   dirs_paths <- file.path(working_directory_path, dirs)
   walk(dirs_paths, ~if (!dir_exists(.x)) dir_create(.x))
   
-  # Load data
+  # Load data: Classified T cell Track data From Behav3D
   master_clust_Live <- readRDS(classified_tcell_track_data_filepath_rds)
   
-  # Generate timepoint ranges
+  # From the %T cell engagement vs Time graph we identified the first targeted time point to find out
+  # the Behavioral signatures frequency for each Experimental Engagement population. 
+  # After determining the first time point you can setup the reoccurring time, which will automatically calculated the the next time point interval for which the following estimation is to be done, as it was done for the first time point.  
+  # Generate time point ranges
   time_intervals <- list()
   for (i in 1:n_timepoints) {
     interval_start <- first_timepoint_intervals[1] + (i - 1) * reoccuring_time
@@ -26,6 +34,7 @@ population_separation_simulation <- function(working_directory_path, classified_
   
   # Define color palette
   color_palette <- c("gold", "darkolivegreen", "seagreen", "forestgreen", "dodgerblue", "cyan", "indianred", "firebrick", "brown")
+  Behavioral_signatures_scale_values <- c("Dead", "Static", "Lazy", "Slow scanner", "Medium scanner", "Super scanner", "Tickler", "Engager", "Super-engager")
   
   # Process data and plot for each time interval
   plots <- future_map(time_intervals, function(interval) {
@@ -34,18 +43,21 @@ population_separation_simulation <- function(working_directory_path, classified_
     
     cd8_data <- subset_data %>% filter(str_detect(tcell_line, "CD8"), cluster != "1")
     cd4_data <- subset_data %>% filter(str_detect(tcell_line, "CD4"), cluster != "1")
-    
+   
+    cd8_data$cluster <- factor(cd8_data$cluster, levels = 1:9, labels = Behavioral_signatures_scale_values)
+    cd4_data$cluster <- factor(cd4_data$cluster, levels = 1:9, labels = Behavioral_signatures_scale_values)
+     
     plot_cd8 <- ggplot(cd8_data, aes(x = factor(contact), fill = factor(cluster))) +
       geom_bar(position = "fill") +
-      scale_fill_manual(values = color_palette) +
-      labs(y = "Percentage", x = "Contact", title = sprintf("CD8 Engagement between timepoint %s", paste(interval, collapse = "-"))) +
+      scale_fill_manual(values = color_palette, name="Behavioral Signatures") +
+      labs(y = "Percentage", x = "CD8 T-cell Contact", title = sprintf("CD8 Engagement between timepoint %s", paste(interval, collapse = "-"))) +
       theme_minimal() +
       coord_flip() +
       facet_wrap(~tcell_line)
     print(plot_cd8)
     
     plot_cd4 <- ggplot(cd4_data, aes(x = factor(contact), fill = factor(cluster))) +
-      geom_bar(position = "fill") +
+      geom_bar(position = "fill", name="Behavioral Signatures") +
       scale_fill_manual(values = color_palette) +
       labs(y = "Percentage", x = "Contact", title = sprintf("CD4 Engagement between timepoint %s", paste(interval, collapse = "-"))) +
       theme_minimal() +
@@ -81,4 +93,8 @@ population_separation_simulation <- function(working_directory_path, classified_
 }
 
 # Example usage of the function
-population_separation_simulation("~/Desktop/working folder/NaPo_bgt/13T_processing/Test", "~/Desktop/working folder/NaPo_bgt/13T_processing/classified_tcell_track_data.rds", 4, c(86, 95), 30)
+population_separation_simulation("~/Desktop/working folder/NaPo_bgt/13T_processing/Test",
+                                 "~/Desktop/working folder/NaPo_bgt/13T_processing/classified_tcell_track_data.rds",
+                                 4,
+                                 c(86, 95),
+                                 30)
